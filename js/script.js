18f1b6f78e4f99fca488a12a2c72333f5269406d
A-MOHAN-VAMSI <2400032078@kluniversity.in>
2026-06-13 21:12:16 +0530

    initiate all

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+const contactForm = document.getElementById("contactForm");
+
+if(contactForm){
+
+    contactForm.addEventListener("submit", function(e){
+
+        e.preventDefault();
+
+        const name = document.getElementById("name").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const subject = document.getElementById("subject").value.trim();
+        const message = document.getElementById("message").value.trim();
+
+        if(name === "" || email === "" || subject === "" || message === ""){
+            alert("Please fill all fields.");
+            return;
+        }
+
+        const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+        if(!email.match(emailPattern)){
+            alert("Please enter a valid email.");
+            return;
+        }
+
+        alert("Message sent successfully!");
+        contactForm.reset();
+    });
+
+}
